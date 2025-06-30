a46cabd5dbcbc38746d6262ada6df3d5c467b297
Rishi Mohan <rishimo@outlook.com>
2025-06-30 18:30:43 +0530

    basic string compression (aabbbcf -> 2a3b1c1f) uncompression still needed

---
+++ Learning/src/StringCompression.java
@@ -0,0 +1,44 @@
+/*pg 91 
+Implement a method to perform basic string compression using counts of repeated characters. If the compressed string is longer than the 
+uncompressed string, return the uncompressed string. Assume that the string has only uppercase and lowercase letters from a-z
+*/
+
+import java.util.Scanner;
+
+public class StringCompression {
+    private static String encode(String input) {
+        String compressed = "";
+        int i = 0;
+        while(compressed.length() < input.length() && i<input.length()-1) {
+            if (input.charAt(i)==input.charAt(i+1)) {
+                int k = i+1;
+                int count = 1;
+                while(k < input.length() && input.charAt(i)==input.charAt(k)) {
+                    count++;
+                    k++;
+                }
+                compressed = compressed + input.charAt(i) + count;
+                i = i + count - 1;
+            }
+            else {
+                compressed += input.charAt(i);
+            }
+            i++;
+        }
+        return compressed;
+    }
+    private static String decode(String input) {
+        String decompressed = "";
+        
+        return decompressed;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        
+
+        System.out.println(encode(input));
+        scanner.close();
+
+    }
+}
