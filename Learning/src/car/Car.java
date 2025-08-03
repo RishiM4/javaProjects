a2dd3b2593b7450caa7dc76e8495c1bc4e221073
Rishi Mohan <rishimo@outlook.com>
2025-08-03 19:31:32 +0530

    Started work on the car class, as of now only supports wheels.

---
+++ Learning/src/car/Car.java
@@ -0,0 +1,20 @@
+package car;
+
+import car.parts.Wheel;
+
+
+
+public class Car {
+    private Wheel wheel;
+    public Wheel getWheel() {
+        return wheel;
+    }
+    public void setWheel(Wheel wheel) {
+        this.wheel = wheel;
+    }
+    public static void main(String[] args) {
+        
+
+        
+    }
+}
---
+++ Learning/src/car/parts/Wheel.java
@@ -0,0 +1,24 @@
+package car.parts;
+
+public class Wheel {
+    private int wheelDiameter;
+    private String brand;
+    private int tirePressure;
+    public Wheel (int diameter, String brand) {
+        this.wheelDiameter = diameter;
+        this.brand = brand;
+    }
+    public int getWheelDiameter() {
+        return wheelDiameter;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setTirePressure(int tirePressure) {
+        this.tirePressure = tirePressure;
+    }
+    public int getTirePressure() {
+        return tirePressure;
+    }
+
+}
